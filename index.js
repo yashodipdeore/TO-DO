c7cdedb7dcc60426920662957cfe8d32dfc85273
yashodip deore <yashodipdeore2006@gmail.com>
2026-06-11 18:05:50 +0530

    Created Public dir and style.css to style pages and form

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
-
+import path from 'path'
 
 //==============================================
 const app = express();
-
 app.set('view engine', 'ejs');
+
+
+const publicPath = path.resolve('public');
+console.log(publicPath);
+
+app.use(express.static(publicPath));
+
 //===============================================
 
 app.get('/', (req, res) => {
@@ -19,6 +25,18 @@ app.get('/add', (req, res) => {
 app.get('/update', (req, res) => {
   res.render('update');
 });
+
+
+
+//-------- POST -----------
+app.post('/add', (req, res) => {
+  res.redirect('/');
+});
+
+app.post('/update', (req, res) => {
+  res.redirect('/');
+});
+
 
 //===============================================
 const PORT = 3000;
